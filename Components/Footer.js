d4086a8e6647ed10bc368adad69a2c9ab1d75bad
Nilesh Deore <nileshdeore2623@gmail.com>
2024-04-28 13:48:58 +0530

    Initial commit

---
+++ Components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-sky-400 flex'>Footer</div>
+  )
+}
+
+export default Footer
---
+++ Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    <div className='bg-sky-400 flex'>
+        <Link href ={`/`}>Navbar</Link>
+
+        <div className='mx-2'>
+            <Link href= {`/productlist`}>Product List</Link>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
